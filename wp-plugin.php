01db1ab864b9d34feef1987f496fdc759648d094
thirdpresence <thirdpresence@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-25 06:07:49 +0000

    git-svn-id: https://plugins.svn.wordpress.org/thirdpresence-for-wordpress/trunk@388758 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wp-plugin.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: ThirdPresence Plugin for Wordpress
+Version: 0.1
+Plugin URI: http://wordpress.org/extend/plugins/thirdpresence/
+Author: ThirdPresence Ltd.
+Author URI: http://wordpress.org/extend/plugins/thirdpresence/
+*/
+
+?>
